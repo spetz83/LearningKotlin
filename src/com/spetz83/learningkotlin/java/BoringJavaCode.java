f7b179c85bd0bfa0e37e4b846324e25ce811c253
Tom Etzel <tom.etzel@quantum.com>
2018-12-01 16:08:54 -0600

    initial commit

---
+++ src/com/spetz83/learningkotlin/java/BoringJavaCode.java
@@ -0,0 +1,7 @@
+package com.spetz83.learningkotlin.java;
+
+public class BoringJavaCode {
+    public static void main(String args[]) {
+        String name = "Tom";
+    }
+}
---
+++ src/com/spetz83/learningkotlin/kotlin/ExploringVariables.kt
@@ -0,0 +1,8 @@
+package com.spetz83.learningkotlin.kotlin
+
+fun main(args: Array<String>) {
+    var name = "Tom"
+    val surname = "Etzel"
+
+    println(name + " " + surname)
+}
